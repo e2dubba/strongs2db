8ccb40033749d4a52a702c0455f20b3f0091a9fa
e2dubba <svirgilgoodwin@gmail.com>
2016-08-08 16:30:46 -0500

    Made some formatting changes

--- strong.py
+++ strong.py
@@ -7,6 +7,7 @@ import random
 conn = sqlite3.connect('strongs.db')
 c = conn.cursor()
 
+
 def create_table():
     c.execute('CREATE TABLE IF NOT EXISTS new_strong_combined (strongs TEXT, greek TEXT, beta TEXT, stnumbers TEXT)')
 
@@ -15,15 +16,13 @@ def data_entry(strongs, greek, beta, stnumbers):
     c.execute('INSERT INTO new_strong_combined VALUES (?, ?, ?, ?)', 
             (strongs, greek, beta, stnumbers))
     conn.commit()
-
-
-#def dynamic_data_entry():
     
 
 xmldoc = minidom.parse('strongsgreek2.xml')
 entries = xmldoc.getElementsByTagName('entries')[0]
 entry = entries.getElementsByTagName('entry')
 create_table() 
+
 
 for item in entry:
     try:
